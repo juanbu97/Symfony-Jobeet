d7007cc00a0c66f03d4c772598a184fc01bb643a
juanbu97 <juanbu97@gmail.com>
2020-02-14 12:51:52 +0100

    Jobeet

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -58,26 +58,46 @@ class CategoryController extends AbstractController
 
     /**
      * Edit category
-     * 
+     *
      * @Route("/admin/category/{id}/edit", name="category.edit", methods={"GET","POST"} , requirements={"id" = "\d+"})
      * @param Request $request
      * @param Category $category
      * @param EntityManagerInterface $em
      * @return Response
      */
-  public function edit(Request $request, Category $category, EntityManagerInterface $em) : Response
-  {
-      $form =  $this->createForm(CategoryType::class, $category);
-      $form->handleRequest($request);
-      if ($form->isSubmitted() && $form->isValid()) {
-          
-          $em->persist($category); //Doctrine administra el objeto
-          $em->flush();
-          return $this->redirectToRoute('category.list');
-      }
-      return $this->render('Admin/category/edit.html.twig', [
+    public function edit(Request $request, Category $category, EntityManagerInterface $em) : Response
+    {
+        $form =  $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($category); //Doctrine administra el objeto
+            $em->flush();
+            return $this->redirectToRoute('category.list');
+        }
+        return $this->render('Admin/category/edit.html.twig', [
             'form'=> $form->createView(),
             'category' => $category
             ]);
-  }
+    }
+
+    /**
+     * Delete category.
+     *
+     * @Route("admin/category/{id}/delete", name="admin.category.delete", methods="DELETE",
+     * requirements={"id" = "\d+"})
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param Category $category
+     *
+     * @return Response
+     */
+    public function delete(Request $request, EntityManagerInterface $em, Category $category) : Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $category->getId(), $request->request->get('_token'))) {
+            $em->remove($category);
+            $em->flush();
+        }
+        return $this->redirectToRoute('category.list');
+    }
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -62,7 +62,7 @@ class Category{
     /**
      * @var Job[]|ArrayCollection
      *
-     * @ORM\OneToMany(targetEntity="Job", mappedBy="category")
+     * @ORM\OneToMany(targetEntity="Job", mappedBy="category", cascade={"remove"})
      */
     private $jobs;
 
